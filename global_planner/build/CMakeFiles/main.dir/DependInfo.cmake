0fe43c6b5e84dae4ba7539505c7f967be3beed21
Justin Nguyen <jnguyenblue2804@gmail.com>
2023-07-30 13:57:53 -0400

    path planning done

--- global_planner/build/CMakeFiles/main.dir/DependInfo.cmake
+++ global_planner/build/CMakeFiles/main.dir/DependInfo.cmake
@@ -4,21 +4,31 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/home/justin/coding_projects/dstar_projects/trajectory_planning/global_planner/main.cpp" "/home/justin/coding_projects/dstar_projects/trajectory_planning/global_planner/build/CMakeFiles/main.dir/main.cpp.o"
+  "/home/justin/coding_projects/dstar_projects/trajectory_planning/global_planner/sim_astar.cpp" "/home/justin/coding_projects/dstar_projects/trajectory_planning/global_planner/build/CMakeFiles/main.dir/sim_astar.cpp.o"
   "/home/justin/coding_projects/dstar_projects/trajectory_planning/global_planner/src/Astar.cpp" "/home/justin/coding_projects/dstar_projects/trajectory_planning/global_planner/build/CMakeFiles/main.dir/src/Astar.cpp.o"
   "/home/justin/coding_projects/dstar_projects/trajectory_planning/global_planner/src/GridMap.cpp" "/home/justin/coding_projects/dstar_projects/trajectory_planning/global_planner/build/CMakeFiles/main.dir/src/GridMap.cpp.o"
   "/home/justin/coding_projects/dstar_projects/trajectory_planning/global_planner/src/PositionVector.cpp" "/home/justin/coding_projects/dstar_projects/trajectory_planning/global_planner/build/CMakeFiles/main.dir/src/PositionVector.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "MATPLOT_BUILD_HIGH_RESOLUTION_WORLD_MAP"
+  "NOMINMAX"
+  )
+
 # The include file search paths:
 set(CMAKE_CXX_TARGET_INCLUDE_PATH
   "/usr/include/eigen3"
   "../include"
+  "_deps/matplotplusplus-src/source"
+  "_deps/matplotplusplus-build/source/matplot"
   )
 
 # Targets to which this target links.
 set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/justin/coding_projects/dstar_projects/trajectory_planning/global_planner/build/_deps/matplotplusplus-build/source/matplot/CMakeFiles/matplot.dir/DependInfo.cmake"
+  "/home/justin/coding_projects/dstar_projects/trajectory_planning/global_planner/build/_deps/matplotplusplus-build/source/3rd_party/CMakeFiles/nodesoup.dir/DependInfo.cmake"
   )
 
 # Fortran module output directory.
